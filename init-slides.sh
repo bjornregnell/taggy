47f12b5c8164677236fe00fc796c378ade0bc847
bjornregnell <bjorn.regnell@cs.lth.se>
2022-09-10 20:52:29 +0200

    improve init-slides.sh to not overwrite

--- init-slides.sh
+++ init-slides.sh
@@ -1,5 +1,18 @@
-#!/bin/bash -v
-cat > slides.scala<< EOF
+#!/bin/bash
+
+DEST=slides.scala
+
+if [ -f "$DEST" ]; then
+  echo "file $DEST already exists"
+  read -p "Enter another file name (or just enter to exit): " DEST
+  if test -z "$DEST"; then
+    echo "No file given. Exiting without creating anything."
+    exit 0
+  fi
+  echo "Creating $DEST"
+fi
+
+cat > "$DEST" << EOF
 // Two magic comments used by scala-cli (just copy-paste them):
 //> using lib "taggy:taggy:0.0.2,url=https://github.com/bjornregnell/taggy/releases/download/v0.0.2/taggy_3-0.0.2.jar"
 //> using scala "3.2.nightly"
@@ -20,5 +33,9 @@ def slides = document("Taggy Slide Example"):
       p("Swedish: Hej världen!")
     p("https://github.com/bjornregnell/taggy")
 EOF
-scala-cli run .
+
+echo "Compiling and running $DEST using scala-cli"
+scala-cli run "$DEST"
+
+echo "Opening pdf-file in target"
 xdg-open target/out.pdf
