3ccd6857c14eac9e798491683221ffcdeea212b4
bjornregnell <bjorn.regnell@cs.lth.se>
2024-05-12 18:47:29 +0200

    bump versions

--- example.scala
+++ example.scala
@@ -1,5 +1,5 @@
-//> using scala "3.3.0-RC3"
-//> using lib "taggy:taggy:0.0.5,url=https://github.com/bjornregnell/taggy/releases/download/v0.0.5/taggy_3-0.0.5.jar"
+//> using scala "3.4.1"
+//> using dep "taggy:taggy:0.0.5,url=https://github.com/bjornregnell/taggy/releases/download/v0.0.5/taggy_3-0.0.5.jar"
 
 //  run in terminal: scala-cli run example.scala
 
